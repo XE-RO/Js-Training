2cdb7fd93ce464fc76abd60c921da86815441fad
XE-RO <delgadox0@gmail.com>
2023-10-03 15:28:56 -0600

    1.0.0

--- Clear 2.js
+++ Clear 2.js
@@ -163,6 +163,9 @@
 // let b =new Number(1)
 // let c=7.19
 // let d="5.65"
+
+//                                                                 
+//                                                                                Try Catch
 // console.log(c+Number.parseInt(b))
 // try{
 //     let numero="1ads"
@@ -173,15 +176,217 @@
 //     console.log(`Se produjo el siguiente ${error}`)
 // }
 
-let numeros=[1,2,3]
 
-let[one,two,three]=numeros
+//                                                                            Destructuracion
 
-console.log(two)
+// let numeros=[1,2,3]
 
-const persona={nombre:"Xavier",apellido:"Delgado",edad:22}
+// let[one,two,three]=numeros
 
-let {nombre,edad,apellido}=persona
+// console.log(two)
 
-console.log(edad)
+// const persona={nombre:"Xavier",apellido:"Delgado",edad:22}
 
+// let {nombre,edad,apellido}=persona
+
+// console.log(edad)
+
+//                                                                          Objetos literales
+        
+//   let name=`Kenai`,age=7
+
+//   const dog1={
+//     name:name,
+//     age:age,
+//     bark:function(){
+//         return (`Guau guau!!!`)
+//     }
+//   }
+//   const dog2={
+//     name,age,race:"chihuahua",bark1(){
+//         return "guau guau Guau!!!"
+//     }
+//   }
+//   console.log(dog2.bark1())
+                 
+//                                                                       Rest Parameters
+// function sumar(a,b,...c){
+//     let resultado = a + b
+//     c.forEach((x)=>{
+//         resultado+=x
+//     })
+// //console.log(c)
+//     return resultado
+// }
+
+// console.log(sumar(2,3,4,5,6,))
+
+//                                                                     Spread operator
+
+// let arr1=[1,2,3,4,5],
+//     arr2=[6,7,8,9,0]
+
+//     let arr3=[...arr1,...arr2]
+//     console.log(arr1,arr2)
+
+//     console.log(arr3)
+
+//                                                                       Arrow functions (pueden cambiar el contexto)
+
+// const perro={
+//     nombre:"Kenai",ladrar(){                                             
+//         console.log(this)   
+//     }
+// }
+// const perro2={
+//     nombre:"Kenai",ladrar:()=>{
+//         console.log(this)
+//     }
+// }
+// console.log(perro.ladrar())
+
+//                                                                               Prototipos
+
+
+// function Animales(name,gender){
+//     this.name=name
+//     this.gender=gender
+// }
+
+
+// let animal1=new Dog("Perro","H","Medium")
+// let animal2=new Animales("Gato","M")
+
+// Animales.prototype.greeting=function(){
+//     return `Hola soy un ${this.name}`
+// }
+// Dog.prototype.sound=function(){
+//     return `Hago sonidos porque estoy vivo`
+// }
+// console.log(animal1.greeting())
+// console.log(animal2.greeting())
+// console.log(animal1.sound())
+// console.log(animal2.sound())
+
+//                                                                           Herrencia prototipica
+// function Dog(name,gender,size){
+//     this.super=Animales
+//     this.super(name,gender)
+//     this.size=size
+// }
+// Dog.prototype=new Animales()
+// Dog.prototype.constructor=Dog
+// Dog.prototype.sound=function(){
+//     return `Soy un perro *Ladrido*`
+// }
+// Dog.prototype.bark=function(){
+//     return `Guau Guau!!`
+// }
+// console.log(animal1.sound())
+
+// //                                                                                   Clases
+
+
+// class Animal{
+// //Atributos
+//     constructor(name,gender){
+//     this.name=name;
+//     this.gender=gender;
+// }
+// // Metodos
+// sound(){
+//     return `I make sounds because i´m alive`
+// }
+// greeting(){
+//     return `Hi my name is ${this.name}`
+// }
+// }
+// let Mimi=new Animal("Mimi","Female")
+// let Scooby=new Animal("Scooby","Male")
+
+
+// console.log(`${Mimi.greeting()} ${Mimi.sound()}`)
+// console.log(`${Scooby.greeting()} ${Scooby.sound()}`)
+
+// class Dog extends Animal{
+//     constructor(name,gender,size){
+//         super(name,gender);
+//         this.size=size
+//         this.breed=null
+//     }
+//     sound(){
+//         return `I'm a dog my sound is a bark`
+//     }
+//     bark(){
+//         return `guau Guau!!`
+//     }
+//     static description(){
+//         return 'I´m a dog and we are the man´s best friend'
+//     }
+//     //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: Getters & Setters
+//     get getBreed(){
+//         return this.breed
+//     }
+//     set setBreed(breed){
+//         this.breed=breed;
+
+//     }
+// }
+// Scooby=new Dog("Scooby","Male","Giant")    
+// console.log(Scooby.bark())
+// console.log(Scooby.sound())
+// console.log(Scooby)
+// console.log(Dog.description())
+
+// console.log(Scooby.getBreed)
+// Scooby.setBreed=`Gran danés`
+// console.log(Scooby.getBreed)
+
+
+//                                                                                                Objeto Console
+
+// console.error(`Esto es un error`)
+// console.warn(`Esto es un aviso`)
+// console.info(`Esto es in info`)
+// console.log("mensaje normal en consola")
+
+// console.group(`First Group`)
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.groupEnd()
+
+
+// console.table(Object.entries(console).sort())
+
+// const numbers=[1,2,3,4,5],
+// vowels=["a","e","i","o","u"]
+// console.table(numbers)
+// console.table(vowels)
+// const Dog={
+//     Name:"Bonie",
+//     Breed:"Boxer",
+//     color:"Brown"
+// }
+// console.table(Dog)
+
+// console.time(`How much time my code takes`)
+// const array1=Array(1000000)
+
+// for(let i=0 ; i<array1.length ; i++){
+//     array1[i]=i
+// }
+// console.timeEnd(`How much time my code takes`)
+
+///////////////
+
+// for(let i=0 ; i<=100 ; i++){
+//     console.count(`For code`)
+//     console.log(i)
+// }
+
+
+let x=3,
+y=2,
+testXY="X must be small than Y";
+console.assert(x<y,{x,y,testXY})
