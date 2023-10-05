0256ac75bb129eae63c2c32617b1241218bfa2f2
XE-RO <delgadox0@gmail.com>
2023-10-05 06:48:53 -0600

    conexion de modulos js

---
+++ Modulos/constantes.js
@@ -0,0 +1,13 @@
+
+export let PI=Math.PI;
+export let usuario=`Xavier`;
+export let password=`qwerty`;
+
+export function Saludar(){
+    console.log(`Hi from Constantes.js`)
+};
+export default function greeting(){
+    console.log(`I,m a default from constantes.js`)
+};
+
+
