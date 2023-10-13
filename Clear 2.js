cb7b95fd2945b8372129cf67eacbfb5f704209c0
XE-RO <delgadox0@gmail.com>
2023-10-12 18:34:17 -0600

    1.0.3

--- Clear 2.js
+++ Clear 2.js
@@ -895,17 +895,17 @@
 
 //                                                                                    JSON
 
-console.log(JSON.parse("{}"))
-console.log(JSON.parse("[1,2,3]"))
-console.log(JSON.parse("true"))
-console.log(JSON.parse("false"))
-console.log(JSON.parse("19"))
-console.log(JSON.parse("null"))
-console.log(JSON.stringify({}))
-console.log(JSON.stringify([]))
-console.log(JSON.stringify(true))
-console.log(JSON.stringify(false))
-console.log(JSON.stringify(undefined))
-console.log(JSON.stringify({x:2,y:3}))
+// console.log(JSON.parse("{}"))
+// console.log(JSON.parse("[1,2,3]"))
+// console.log(JSON.parse("true"))
+// console.log(JSON.parse("false"))
+// console.log(JSON.parse("19"))
+// console.log(JSON.parse("null"))
+// console.log(JSON.stringify({}))
+// console.log(JSON.stringify([]))
+// console.log(JSON.stringify(true))
+// console.log(JSON.stringify(false))
+// console.log(JSON.stringify(undefined))
+// console.log(JSON.stringify({x:2,y:3}))
 
 
